c4b2b97ebf4e6ffe3ff36f04e5ece054363425d2
DESKTOP-LKF81KV\Mak Moinee <titus.silver.ph@gmail.com>
2022-10-11 02:00:02 +0800

    update profile

--- app/src/main/java/com/aclc/thesis/jmsapp/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/aclc/thesis/jmsapp/ui/profile/ProfileFragment.java
@@ -3,9 +3,11 @@ package com.aclc.thesis.jmsapp.ui.profile;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +15,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.aclc.thesis.jmsapp.R;
+import com.aclc.thesis.jmsapp.models.Users;
+import com.aclc.thesis.jmsapp.models.Visitor;
 import com.aclc.thesis.jmsapp.preference.UserPreference;
 import com.aclc.thesis.jmsapp.preference.UserPreferenceImpl;
 import com.aclc.thesis.jmsapp.preference.VisitorPreference;
@@ -23,7 +27,7 @@ public class ProfileFragment extends Fragment {
 
     private Context mContext;
     private ImageView imgProfile;
-    private TextView txtAccName, txtUsername;
+    private TextView txtAccName, txtUsername, txtAddress, txtBplace, txtFN, txtMN, txtLN, txtBdate;
     private VisitorPreference visitorPreference;
     private UserPreference userPreference;
 
@@ -40,6 +44,13 @@ public class ProfileFragment extends Fragment {
         initViews(mView);
         String username = userPreference.getUsers().getUserName() != null ? userPreference.getUsers().getUserName() : "";
         txtUsername.setText(username);
+        Visitor visitor = visitorPreference.getVisitor();
+        txtLN.setText(visitor.getLastName());
+        txtMN.setText(visitor.getMiddleName());
+        txtFN.setText(visitor.getFirstName());
+        txtAddress.setText(visitor.getAddress());
+        txtBplace.setText(visitor.getBirthPlace());
+        txtBdate.setText(visitor.getBirthDate());
         String fullName = visitorPreference.getFullName() != null ? visitorPreference.getFullName() : "";
         txtAccName.setText(fullName);
         return mView;
@@ -49,6 +60,12 @@ public class ProfileFragment extends Fragment {
         imgProfile = mView.findViewById(R.id.imgProfile);
         txtAccName = mView.findViewById(R.id.txtAccName);
         txtUsername = mView.findViewById(R.id.txtAccUN);
+        txtAddress = mView.findViewById(R.id.txtAddress);
+        txtBplace = mView.findViewById(R.id.txtBplace);
+        txtFN = mView.findViewById(R.id.txtFN);
+        txtMN = mView.findViewById(R.id.txtMN);
+        txtLN = mView.findViewById(R.id.txtLN);
+        txtBdate = mView.findViewById(R.id.txtBdate);
     }
 
     @Override
